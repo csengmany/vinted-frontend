81effa1a0e0cd4369afc33d09ec62303e5b8d2ad
csengmany <sengmany.cathy@gmail.com>
2021-02-10 15:35:53 +0100

    create Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,13 @@ const Header = () => {
             <div>
                 <img src={logo} alt="logo vinted" />
             </div>
+
+            <div>
+                <button>S'inscrire</button>
+                <button>Se connecter</button>
+            </div>
+
+            <button>Vends tes articles</button>
         </div>
     );
 };
