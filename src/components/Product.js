7e8049f856cbe6edf8590f0aaba9723d4a05b4fe
csengmany <sengmany.cathy@gmail.com>
2021-02-10 22:27:12 +0100

    update css of Product

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,33 +3,33 @@ import { Link } from "react-router-dom";
 const Product = ({ id, offer }) => {
     return (
         // if click on product it will link to the offer page
-        <Link to={`/offer/${id}`}>
-            <div className="product">
-                <div className="product-owner">
-                    {offer.owner.account.avatar && (
-                        <span className="product-owner-avatar">
-                            <img
-                                src={offer.owner.account.avatar.secure_url}
-                                alt=""
-                            />
-                        </span>
-                    )}
 
-                    <span>{offer.owner.account.username}</span>
-                </div>
+        <div className="product">
+            <div className="product-owner">
+                {offer.owner.account.avatar && (
+                    <span className="product-owner-avatar">
+                        <img
+                            src={offer.owner.account.avatar.secure_url}
+                            alt=""
+                        />
+                    </span>
+                )}
 
+                <span>{offer.owner.account.username}</span>
+            </div>
+
+            <Link to={`/offer/${id}`} className="product-link">
                 <span className="product-img">
                     <img src={offer.product_image.secure_url} alt="" />
                 </span>
-
                 <div className="product-details">
                     <span>{offer.product_price} €</span>
 
                     <span>{offer.product_details[1].TAILLE}</span>
                     <span>{offer.product_details[0].MARQUE}</span>
                 </div>
-            </div>
-        </Link>
+            </Link>
+        </div>
     );
 };
 export default Product;
